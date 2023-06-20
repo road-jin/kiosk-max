cd9b0fa4c912e7d4d7a58cebe7fa5efd0438d634
n-aa-g <wlsdnjs0124@gmail.com>
2023-06-20 19:35:27 +0900

    Build: 스프링 프로젝트 생성

---
+++ be/src/main/java/kr/codesquad/kioskmax/KioskMaxApplication.java
@@ -0,0 +1,13 @@
+package kr.codesquad.kioskmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskMaxApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/kr/codesquad/kioskmax/KioskMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad.kioskmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
