30926741c092c975705ba0bf258eb55a19942150
road-jin <rjswmtk@gmail.com>
2023-06-21 22:03:07 +0900

    feat: 카테고리 조회 기능 추가

---
+++ be/src/test/java/kr/codesquad/kioskmax/repository/CategoryRepositoryTest.java
@@ -0,0 +1,36 @@
+package kr.codesquad.kioskmax.repository;
+
+import java.util.List;
+import javax.sql.DataSource;
+import kr.codesquad.kioskmax.domain.Category;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@JdbcTest
+@ActiveProfiles("test")
+class CategoryRepositoryTest {
+
+    private final CategoryRepository repository;
+
+    @Autowired
+    public CategoryRepositoryTest(DataSource dataSource) {
+        this.repository = new CategoryRepository(dataSource);
+    }
+
+    // AAA일 때, B를 하면 C가 된다.
+    @Test
+    @DisplayName("카테고리가 있을 때 카테고리 전체 조회하면 모든 카테고리를 반환한다.")
+    void findAll() {
+        // given
+
+        // when
+        List<Category> actual = repository.findAll();
+
+        // then
+        Assertions.assertThat(actual.size()).isEqualTo(9);
+    }
+}
