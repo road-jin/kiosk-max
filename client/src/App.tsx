544f19f6f0636b6c69bfda83827f7b75d531022f
Jello <60080167+hjsong333@users.noreply.github.com>
2023-06-22 22:09:19 +0900

    메뉴 담기 모달 구현 (#15)
    * feat #13: 메뉴 담기 모달 구현
    
    - App 컴포넌트의 하위 컴포넌트로 CartAdditionModal 컴포넌트를 추가
    - 개별 메뉴를 클릭하면 CartAdditionModal 컴포넌트가 나타나도록 구현
    - Backdrop 클릭하면 CartAdditionModal 컴포넌트가 사라지도록 구현
    
    * feat #13: 메뉴 선택 시 메뉴 담기 모달로 정보 전달
    
    - selectedMenu state를 추가하여 선택한 메뉴 정보 저장
    - 메뉴 선택 시 메뉴 담기 모달로 정보 전달
    - 메뉴 담기 모달에서 선택한 메뉴 정보를 받아와서 장바구니에 추가
    - 메뉴 담기 모달 레이아웃 작업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import style from "./App.module.css";
+import CartAdditionModal from "./components/CartAdditionModal";
 import CategoryTab from "./components/CategoryTab";
 import MenuList from "./components/MenuList";
 
@@ -11,6 +12,8 @@ interface Beverage {
 export default function App() {
   const [beverages, setBeverages] = useState<Beverage[]>([]);
   const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
+  const [isCartAdditionModalOpen, setIsCartAdditionModalOpen] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState<Menu | null>(null);
 
   const categories = beverages.map((beverage) => beverage.category) ?? [];
   const currentMenus = beverages[currentCategoryIndex]?.menus ?? [];
@@ -42,6 +45,15 @@ export default function App() {
     setCurrentCategoryIndex(index);
   };
 
+  const handleMenuItemClick = (menu: Menu) => {
+    setSelectedMenu(menu);
+    setIsCartAdditionModalOpen(true);
+  };
+
+  const handleBackdropClick = () => {
+    setIsCartAdditionModalOpen(false);
+  };
+
   return (
     <div className={style.App}>
       <CategoryTab //
@@ -49,7 +61,8 @@ export default function App() {
         currentCategoryIndex={currentCategoryIndex}
         handleCategoryChange={handleCategoryChange}
       />
-      <MenuList key={currentCategoryIndex} menus={currentMenus} />
+      <MenuList key={currentCategoryIndex} menus={currentMenus} handleMenuItemClick={handleMenuItemClick} />
+      {isCartAdditionModalOpen && <CartAdditionModal menu={selectedMenu} handleBackdropClick={handleBackdropClick} />}
     </div>
   );
 }
