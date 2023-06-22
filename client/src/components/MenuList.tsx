4fe29f5e1da17a52b671dcf5a0fe04ea93e72b54
khundi <albert5428@gmail.com>
2023-06-23 08:56:31 +0900

    메뉴 리스트 애니메이션 구현 (#22)
    feat #21: 메뉴 리스트 애니메이션 구현

--- client/src/components/MenuList.tsx
+++ client/src/components/MenuList.tsx
@@ -1,18 +1,20 @@
+import { AnimationClass } from "../App";
 import MenuItem from "./MenuItem";
 import style from "./MenuList.module.css";
 
 interface MenuListProps {
   menus: Menu[];
   handleMenuItemClick: (menu: Menu) => void;
+  animation: AnimationClass;
 }
 
-export default function MenuList({ menus, handleMenuItemClick }: MenuListProps) {
+export default function MenuList({ menus, handleMenuItemClick, animation }: MenuListProps) {
   return (
-    <main className={`${style.MenuList} ${style.AnimateIn}`}>
+    <main className={`${style.MenuList} ${style[animation]}`}>
       <ul className={style.MenuListContainer}>
         {menus.map((menu) => (
-          <li className={style.MenuItem} onClick={() => handleMenuItemClick(menu)}>
-            <MenuItem key={menu.id} menu={menu} />
+          <li className={style.MenuItem} key={menu.id} onClick={() => handleMenuItemClick(menu)}>
+            <MenuItem menu={menu} />
           </li>
         ))}
       </ul>
